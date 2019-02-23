32f4005009a5bd698b1e3d9b752d1c0152d7d58a
Lily <44213621+lilysimmons@users.noreply.github.com>
2019-02-22 20:21:48 -0500

    omg

--- config/connection.js
+++ config/connection.js
@@ -13,7 +13,6 @@ else{
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
         database: process.env.DB_NAME
-
 });
 
 }
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Double Toil and Trouble');
+INSERT INTO burgers (burger_name) VALUES ('Vegemite Delight');
+INSERT INTO burgers (burger_name) VALUES ('Ketchup Only');
